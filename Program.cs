e7d9cb67cd40681cd8c3fbe4a3cd2300d16c4d2f
Andres Bernal <danbeac@hotmail.com>
2021-01-27 18:29:28 -0500

    It has added BubbleSort

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace programming_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FuncionAlgBubbleSort();
+        }
+
+        public static void FuncionAlgBubbleSort()
+        {
+            int[] arr = new int[]{10,5,9,7,24,29,2};
+            
+            for(int i = 0 ; i < arr.Length ; i++)
+            {
+                for(int j = 0; j < arr.Length - i- 1;j++)
+                {
+                    if(arr[j] > arr[j+1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
         }
     }
 }
